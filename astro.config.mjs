2487f96137716781cfc66514728aea94bcfbe8e2
Zaccal <ksss90411@gmail.com>
2026-08-20 01:00:03 +0500

    ✨ feat: created about section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "astro/config"
-import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +11,4 @@ export default defineConfig({
   },
   integrations: [react()],
   site: "https://zaccal.github.io",
-})
+});
